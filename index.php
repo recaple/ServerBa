98888330e1f6cae5b0391a9dd41bed2bb4b98932
recaple <vanya.golubtsow@yandex.ru>
2017-05-23 23:38:42 +0300

    Add project

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php
+echo system('/usr/lib/cgi-bin/t.sh');
+echo system('/usr/lib/cgi-bin/t0.sh');
+echo system('/usr/lib/cgi-bin/t1.sh');
+echo system('/usr/lib/cgi-bin/t2.sh');
+echo system('/usr/lib/cgi-bin/t3.sh');
+echo system('/usr/lib/cgi-bin/t4.sh');
+?>
+</body>
+</html>
+
--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 apt-get install -y bc 
-apt-get install -y elinks 
-apt-get install -y apache2
-apt-get install -y sysstat 
-apt-get install -y libapache2-mod-php 
-echo DocumentRoot /var/www/html/ » /etc/apache2/apache2.conf 
-cp /home/recaple/ports.conf /etc/apache2/ports.conf 
-servise apache2 restart 
-apt-get install -y nginx 
-cp /home/recaple/nginx.conf /etc/nginx/nginx.conf 
-servise nginx restart 
-apt-get install -y libapache2-mod-rpaf 
-a2enmod rpaf 
-cp /home/recaple/rpaf.conf /etc/apache2/mods-enabled/rpaf.conf 
-/etc/init.d/httpd restart 
-/etc/init.d/nginx restart 
+apt-get install -y elinks
+apt-get install -y sysstat
+apt-get install nginx -y
+cp default /etc/nginx/sites-available/
+nginx -s reload
+apt-get install apache2 php7.0 libapache2-mod-php7.0 -y
+cp ports.conf /etc/apache2/
+cp 000-default.conf /etc/apache2/sites-available/
+apachectl restart
+rm -rf /var/www/html/*
+mkdir -p /var/www/html/sysinfo
+cp index.php /var/www/html/sysinfo/ 
+cp t.sh /usr/lib/cgi-bin/
+cp t1.sh /usr/lib/cgi-bin/
+cp t2.sh /usr/lib/cgi-bin/
+cp t3.sh /usr/lib/cgi-bin/
+cp t4.sh /usr/lib/cgi-bin/
+cp t0.sh /usr/lib/cgi-bin/
 exit
