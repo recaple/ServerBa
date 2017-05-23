ba7cca24b74fc152d1077a72ae160323c5baaafa
recaple <vanya.golubtsow@yandex.ru>
2017-05-23 20:26:45 +0300

    Add project

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt-get install -y bc 
+apt-get install -y elinks 
+apt-get install -y apache2
+apt-get install -y sysstat 
+apt-get install -y libapache2-mod-php 
+echo DocumentRoot /var/www/html/ » /etc/apache2/apache2.conf 
+cp /home/alex/ports.conf /etc/apache2/ports.conf 
+servise apache2 restart 
+apt-get install -y nginx 
+cp /home/alex/nginx.conf /etc/nginx/nginx.conf 
+servise nginx restart 
+apt-get install -y libapache2-mod-rpaf 
+a2enmod rpaf 
+cp /home/alex/rpaf.conf /etc//apache2/mods-enabled/rpaf.conf 
+/etc/init.d/httpd restart 
+/etc/init.d/nginx restart 
+exit
