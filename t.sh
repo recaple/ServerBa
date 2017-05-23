4db1d69109f14215c99f2c7cd923149c2bba3595
recaple <vanya.golubtsow@yandex.ru>
2017-05-24 01:00:24 +0300

    Update t.sh

--- t.sh
+++ t.sh
@@ -18,15 +18,15 @@ echo "Iostat"
 echo "<br />"
 echo "<tr><th>Device:</th><th>rrqm/s</th><th>wrqm/s</th><th>r/s</th><th>w/s</th><th>rkB/s</th><th>wkB/s</th><th>avgrq-sz"
 echo "</th><th>avgqu-sz</th><th>await</th><th>r_await</th><th>w_await</th><th>svctm</th><th>%util</th></tr>"
-iostat -dkx | sed -e'1,3d' > /home/recaple/io.txt
-k=$(wc -l /home/recaple/io.txt | awk '{print $1;}')
+iostat -dkx | sed -e'1,3d' > /home/recaple/i.txt
+k=$(wc -l /home/recaple/i.txt | awk '{print $1;}')
 count=1
 while [ $count -lt $k ]
 do
 echo "<tr>"
-cat /home/recaple/io.txt | head -n$count | tail -n1 | awk  '{ for(i=1; i<14; i++){ print ("<td>" $i "</td>")}}' 
+cat /home/recaple/i.txt | head -n$count | tail -n1 | awk  '{ for(i=1; i<14; i++){ print ("<td>" $i "</td>")}}' 
 echo "<td>"
-res=$(cat  /home/recaple/io.txt | head -n$count | tail -n1 | awk '{print $14}')
+res=$(cat  /home/recaple/i.txt | head -n$count | tail -n1 | awk '{print $14}')
 pygmentize $res
 echo "</td>"
 echo "</tr>"
