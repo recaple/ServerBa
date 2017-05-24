417fa1f39d0d0358a9d8fb7bbb9ac528d7b06fd1
recaple <vanya.golubtsow@yandex.ru>
2017-05-24 03:55:47 +0300

    Update cronio.sh

--- cronio.sh
+++ cronio.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+for i in {1..4}; do
 iostat -dkx | sed -e'1,3d' > /home/recaple/i.txt
 cat /proc/net/dev | sed -e '1,2d' > /home/recaple/n.txt
 mpstat | sed -e '1,3d' > /home/recaple/d.txt
@@ -9,34 +10,4 @@ df | sed -e '1d' > /home/recaple/t.txt
 #sed -i '/\/var\//d' /home/recaple/test.txt
 #sed -i '/\/proc\//d' /home/recaple/test.txt
 sleep 15
-iostat -dkx | sed -e'1,3d' > /home/recaple/i.txt
-cat /proc/net/dev | sed -e '1,2d' > /home/recaple/n.txt
-mpstat | sed -e '1,3d' > /home/recaple/d.txt
-netstat -nlptu | sed -e '1,2d' > /home/recaple/net.txt
-df -i | sed -e '1d' > /home/recaple/o.txt
-df | sed -e '1d' > /home/recaple/t.txt
-#sed -i '/\/dev\//d' /home/recaple/test.txt
-#sed -i '/\/var\//d' /home/recaple/test.txt
-#sed -i '/\/proc\//d' /home/recaple/test.txt
-sleep 15
-iostat -dkx | sed -e'1,3d' > /home/recaple/i.txt
-cat /proc/net/dev | sed -e '1,2d' > /home/recaple/n.txt
-mpstat | sed -e '1,3d' > /home/recaple/d.txt
-netstat -nlptu | sed -e '1,2d' > /home/recaple/net.txt
-df -i | sed -e '1d' > /home/recaple/o.txt
-df | sed -e '1d' > /home/recaple/t.txt
-#sed -i '/\/dev\//d' /home/recaple/test.txt
-#sed -i '/\/var\//d' /home/recaple/test.txt
-#sed -i '/\/proc\//d' /home/recaple/test.txt
-sleep 15
-iostat -dkx | sed -e'1,3d' > /home/recaple/i.txt
-cat /proc/net/dev | sed -e '1,2d' > /home/recaple/n.txt
-mpstat | sed -e '1,3d' > /home/recaple/d.txt
-netstat -nlptu | sed -e '1,2d' > /home/recaple/net.txt
-df -i | sed -e '1d' > /home/recaple/o.txt
-df | sed -e '1d' > /home/recaple/t.txt
-#sed -i '/\/dev\//d' /home/recaple/test.txt
-#sed -i '/\/var\//d' /home/recaple/test.txt
-#sed -i '/\/proc\//d' /home/recaple/test.txt
-sleep 15
-
+done
